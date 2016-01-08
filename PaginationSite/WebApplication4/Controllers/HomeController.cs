212c2f792e698d15c11a8b408e126e2256b37b0e
Joel Richard Benoit <joel.r.benoit@gmail.com>
2016-01-08 14:24:39 -0800

    Pagination ASP SQL Site
    Working Pagination ASP SQL Site

---
+++ PaginationSite/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Repository;
+using PagedList;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string currentFilter, string sortOrder, string searchString, int? page)
+        {
+            JB_FoodStoreEntities context = new JB_FoodStoreEntities();
+
+            if (searchString != null)
+                page = 1;
+            else
+                searchString = currentFilter;
+
+
+            FoodRespository productRepo = new FoodRespository();
+            IEnumerable<Product> products = productRepo.GetProducts(searchString, sortOrder);
+
+            // Store current sort filter parameter.
+            ViewBag.CurrentFilter = searchString;
+            ViewBag.CurrentSort = sortOrder;
+
+            // Provide toggle option for name sort.
+            if (String.IsNullOrEmpty(sortOrder))
+                ViewBag.NameSortParm = FoodRespository.NAME;
+            else
+                ViewBag.NameSortParm = "";
+
+            // Provide toggle  optionfor date sort.
+            if (sortOrder == FoodRespository.MFGDISCOUNT)
+                ViewBag.DateSortParm = FoodRespository.MFG_DESC;
+            else
+                ViewBag.DateSortParm = FoodRespository.MFGDISCOUNT;
+
+
+            const int PAGE_SIZE = 2;
+            int pageNumber = (page ?? 1);
+            // Truncate results to fit in the view provided.
+            products = products.ToPagedList(pageNumber, PAGE_SIZE);
+            return View(products);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PaginationSite/WebApplication4/Repository/FoodRespository.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Repository
+{
+    public class FoodRespository
+    {
+        public const string PRODUCT_ID = "Productid";
+        public const string NAME = "Name";
+        public const string MANUNAME = "ManuName";
+        public const string MFG = "Mfg";
+        public const string VENDOR = "Vender";
+        public const string PRICE = "Price";
+        public const string MFGDISCOUNT = "Mfgdiscount";
+        public const string DATE_DESC = "Date_desc";
+
+        IEnumerable<Product> SortStudents(IEnumerable<Product> products, string sortOrder)
+        {
+            switch (sortOrder)
+            {
+                case NAME:
+                    products = products.OrderByDescending(s => s.name);
+                    break;
+                case MANUNAME:
+                    products = products.OrderBy(s => s.Manufacturer);
+                    break;
+                case MFGDISCOUNT:
+                    products = products.OrderByDescending(s => s.Manufacturer.mfgDiscount);
+                    break;
+                case PRICE:
+                    products = products.OrderByDescending(s => s.price);
+                    break;
+                case VENDOR:
+                    products = products.OrderByDescending(s => s.vendor);
+                    break;
+                default:
+                    products = products.OrderByDescending(s => s.productID);
+                    break;
+            }
+            return products;
+        }
+
+        IEnumerable<Product> FilterProducts(IEnumerable<Product> products, string searchString)
+        {
+            // Filter results based on search.
+            if (!String.IsNullOrEmpty(searchString))
+                products = products.Where(
+                            s => s.name.ToUpper().Contains(searchString.ToUpper())
+                              || s.mfg.ToUpper().Contains(searchString.ToUpper()));
+            return products;
+        }
+
+        public IEnumerable<Product> GetProducts(string searchString, string sortOrder)
+        {
+            JB_FoodStoreEntities context = new JB_FoodStoreEntities();
+            IEnumerable<Product> products = from s in context.Products
+                                            select s;
+
+            products = FilterProducts(products, searchString);
+            products = SortProducts(products, sortOrder);
+
+            return products;
+        }
+        public const string MFG_DESC = "Mfg_desc";
+
+        IEnumerable<Product> SortProducts(IEnumerable<Product> products, string sortOrder)
+        {
+            switch (sortOrder)
+            {
+                case NAME:
+                    products = products.OrderByDescending(s => s.name);
+                    break;
+                case MANUNAME:
+                    products = products.OrderBy(s => s.Manufacturer);
+                    break;
+                case MFGDISCOUNT:
+                    products = products.OrderByDescending(s => s.Manufacturer.mfgDiscount);
+                    break;
+                case PRICE:
+                    products = products.OrderByDescending(s => s.price);
+                    break;
+                case VENDOR:
+                    products = products.OrderByDescending(s => s.vendor);
+                    break;
+                default:
+                    products = products.OrderByDescending(s => s.productID);
+                    break;
+            }
+            return products;
+        }
+
+        IEnumerable<Product> FilterStudents(IEnumerable<Product> products, string searchString)
+        {
+            // Filter results based on search.
+            if (!String.IsNullOrEmpty(searchString))
+                products = products.Where(
+                            s => s.name.ToUpper().Contains(searchString.ToUpper())
+                              || s.mfg.ToUpper().Contains(searchString.ToUpper()));
+            return products;
+        }
+
+        public IEnumerable<Product> GetStudents(string searchString, string sortOrder)
+        {
+            JB_FoodStoreEntities context = new JB_FoodStoreEntities();
+            IEnumerable<Product> products = from s in context.Products
+                                            select s;
+
+            products = FilterStudents(products, searchString);
+            products = SortStudents(products, sortOrder);
+
+            return products;
+        }
+    }
+}
